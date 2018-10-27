0a574173821e7d8e9cabe3c94f59ff10eba5b5c6
jmatos01 <jmatos@techlaunch.io>
2018-10-27 13:00:59 -0400

    first commit

---
+++ src/cardData.js
@@ -0,0 +1,14 @@
+const cardData = {
+    headline: 'Programming is my language',
+  
+    name: 'John Matos',
+  
+    message: "I am a believer that when you learn how to program, your mindset changes complete. With that said check out my talent!",
+  
+    url: 'jmatos.techlaunch.io',
+  
+    urlDisplayName: 'Click Here!',
+  }
+  
+  export default cardData;
+  
